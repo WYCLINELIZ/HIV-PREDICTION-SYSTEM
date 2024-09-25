1234e3916bdbabd5584083987a9e08e0b8d7fda5
WYCLINELIZ <104576271+WYCLINELIZ@users.noreply.github.com>
2024-09-25 09:35:08 +0300

    Update adh.py

--- adh.py
+++ adh.py
@@ -18,7 +18,7 @@ df['sex'] = df['sex'].map({'M': 1, 'F': 0})
 # Prepare data
 X = df[['sex', 'age', 'weight', 'height']]
 y = df['outcome']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 # Train model
 model = RandomForestClassifier()
